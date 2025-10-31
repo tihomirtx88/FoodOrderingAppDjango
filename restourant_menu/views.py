c3f4d7d1ec1a303d5ecca6794c107d0368f53304
tihomirtx88 <tihomirtx88@gmail.com>
2025-10-31 19:40:43 +0200

    Creating admin interface and add data

--- restourant_menu/views.py
+++ restourant_menu/views.py
@@ -6,6 +6,10 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
+    def get_context_data(self):
+        context = {"meals": ["pizza", "pasta"], "ingredients": ["things"]}
+        return context
+
 class MenuItemDetail(generic.DetailView):
     model = Item
     template_name = "menu_item_detail.html"
